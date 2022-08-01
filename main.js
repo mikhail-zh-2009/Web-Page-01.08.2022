e5db85cc051e292a141271353abf4b26ea6475f9
AAAAAA999999 <sukasukasukasuka999999@gmail.com>
2022-08-01 13:16:26 +0500

    Begin.

---
+++ main.js
@@ -0,0 +1,16 @@
+const cv = document.getElementById('cv');
+const ctx = cv.getContext('2d');
+
+class bubble {
+    constructor(x, y, max_radius) {
+        this.x          = x;
+        this.y          = y;
+        this.radius     = 0;
+        this.max_radius = max_radius;
+    }
+}
+
+function main() {
+    
+}
+setInterval(main, 10);
